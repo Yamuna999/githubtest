a39903c67aa0438cd951068a1f908039251cf6b0
Yamuna <999yamu@gmail.com>
2022-12-06 19:34:02 +0000

    test

---
+++ students.java
@@ -0,0 +1,10 @@
+public class students
+{
+	public static void main(string[]args)
+	{
+		system.out.println("Hello there \nwelcome to devops session");
+		system.out.println("students joined the session");
+		system.out.println("mohamed\nazeez\nvidya\nyamuna\puru\nsai");
+	}
+}
+
